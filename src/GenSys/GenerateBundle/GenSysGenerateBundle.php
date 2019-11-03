7cfffbf4a765a54c532182c16cd37d6ec4c7b52a
tomgroskamp <tom.groskamp@gmail.com>
2019-11-03 09:54:18 +0100

    Ticked off todos

--- src/GenSys/GenerateBundle/GenSysGenerateBundle.php
+++ src/GenSys/GenerateBundle/GenSysGenerateBundle.php
@@ -6,9 +6,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class GenSysGenerateBundle extends Bundle
 {
-    //TODO: instantiate to-test class with dependencies under the method defintions
     //TODO: read out multiple return types on tested method, prepare for multiple assertions.
     //TODO: check return types of called methods, checking if we need to return a mockobject for this.
-    //TODO: add default version of the to test service in the setupmethod.
     //TODO: interactive (1-9 number options) to add tests to existing unit test class, matching which unit test methods are already present.
 }
