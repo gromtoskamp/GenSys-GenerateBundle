e77c825f76b77c3bba3b130abc5dfe0572b90a56
gromtoskamp <tom.groskamp@gmail.com>
2019-11-11 11:31:09 +0100

    Add todo

--- src/GenSysGenerateBundle.php
+++ src/GenSysGenerateBundle.php
@@ -6,4 +6,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class GenSysGenerateBundle extends Bundle
 {
+    //TODO: fix that '$variable = $parameterDepencency->methodCall()' is not picked up as methodcall.
 }
