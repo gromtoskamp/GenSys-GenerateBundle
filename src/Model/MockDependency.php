0770aa5369ba9cc01a9eb89a003250a736634471
gromtoskamp <tom.groskamp@gmail.com>
2019-12-25 23:56:52 +0100

    Docblocks

--- src/Model/MockDependency.php
+++ src/Model/MockDependency.php
@@ -9,22 +9,35 @@ class MockDependency
     /** @var ReflectionParameter */
     private $parameter;
 
+    /**
+     * MockDependency constructor.
+     * @param ReflectionParameter $parameter
+     */
     public function __construct(
         ReflectionParameter $parameter
     ) {
         $this->parameter = $parameter;
     }
 
+    /**
+     * @return string
+     */
     public function getClassName(): string
     {
         return $this->parameter->getClass()->getShortName();
     }
 
+    /**
+     * @return string
+     */
     public function getPropertyName(): string
     {
         return lcfirst($this->getClassName());
     }
 
+    /**
+     * @return string
+     */
     public function getFullyQualifiedClassName(): string
     {
         return $this->parameter->getClass()->getName();
